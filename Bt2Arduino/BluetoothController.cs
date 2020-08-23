8546448ed0176eea38e2072a6093a258a50f9bbe
Maxim-Issaev <m.issaev2000@gmail.com>
2020-08-23 22:40:51 +0300

    First Build

---
+++ Bt2Arduino/BluetoothController.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Android.App;
+using Android.Bluetooth;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+using Java.Util;
+
+namespace Bt2Arduino
+{
+    
+    public class BluetoothController
+    {
+        private static UUID mDeviceUUID;
+        public List<BluetoothDevice> BluetoothDevices;
+        private BluetoothAdapter bluetoothAdapter;
+        private BluetoothSocket socket;
+        public ConncetionSate conncetionState;
+
+        private OutputStreamInvoker outStream = null;
+        private InputStreamInvoker inStream = null;
+
+        public BluetoothController()
+        {
+            mDeviceUUID = Java.Util.UUID.FromString("00001101-0000-1000-8000-00805F9B34FB");
+            bluetoothAdapter = BluetoothAdapter.DefaultAdapter;
+            BluetoothDevices = new List<BluetoothDevice>();
+            ICollection<BluetoothDevice> devices = bluetoothAdapter.BondedDevices;
+            foreach (var device in devices)
+            {
+                BluetoothDevices.Add(device);
+            }
+            conncetionState = ConncetionSate.failed;
+
+        }
+        public async Task ConnectAsync(int TargetId)
+        {
+            socket = BluetoothDevices[TargetId].CreateRfcommSocketToServiceRecord(mDeviceUUID);
+            try
+            {
+                await socket.ConnectAsync();
+                if (socket != null && socket.IsConnected)
+                {
+                    outStream = (OutputStreamInvoker)socket.OutputStream;
+                    conncetionState = ConncetionSate.sucsesful;
+                }
+                else
+                    conncetionState = ConncetionSate.failed;
+
+
+            }
+            catch (Java.IO.IOException)
+            {
+
+                conncetionState = ConncetionSate.failed;
+            }
+        }
+        public async Task WriteAsync(string message)
+        {
+            uint messageLength = (uint)message.Length;
+            byte[] countBuffer = BitConverter.GetBytes(messageLength);
+            byte[] buffer = Encoding.UTF8.GetBytes(message);
+
+            await outStream.WriteAsync(countBuffer, 0, countBuffer.Length);
+            await outStream.WriteAsync(buffer, 0, buffer.Length);
+        }
+        
+    }
+    public enum ConncetionSate
+    {
+        failed,
+        sucsesful
+    }
+
+}
---
+++ Bt2Arduino/MainActivity.cs
@@ -0,0 +1,87 @@
+﻿using Android.App;
+using Android.OS;
+using Android.Support.V7.App;
+using Android.Runtime;
+using Android.Widget;
+using Android.Bluetooth;
+using System.Collections.Generic;
+using Android.Views;
+using System;
+using System.Threading.Tasks;
+using Android.Content;
+
+namespace Bt2Arduino
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
+    public class MainActivity : AppCompatActivity
+    {
+        private int SelectedID=0;
+        
+        private Button SendnButton;
+        private Button ConnectButton;
+        private Spinner Spinner;
+
+        public BluetoothController bluetoothController;
+        
+        
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            bluetoothController = new BluetoothController();
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.activity_main);
+            
+            ConnectButton = FindViewById<Button>(Resource.Id.button1);
+            Spinner = FindViewById<Spinner>(Resource.Id.spinner1);
+            Spinner.ItemSelected += Spinner_ItemSelected1;
+            List<string> names = new List<string>();
+            foreach (BluetoothDevice device in bluetoothController.BluetoothDevices)
+            {
+                names.Add(device.Name);
+            }
+            ArrayAdapter adapter = new ArrayAdapter(this, Android.Resource.Layout.SimpleSpinnerItem, names);
+            Spinner.Adapter = adapter;
+            ConnectButton.Click += ConnectButton_ClickAsync;
+
+            SendnButton = FindViewById<Button>(Resource.Id.button2);
+            SendnButton.Click += SendnButton_ClickAsync;
+            
+        }
+
+        private async void SendnButton_ClickAsync(object sender, EventArgs e)
+        {
+            await bluetoothController.WriteAsync("1");
+        }
+
+        private async void ConnectButton_ClickAsync(object sender, EventArgs e)
+        {
+            if (bluetoothController.conncetionState == ConncetionSate.failed)
+            {
+                await bluetoothController.ConnectAsync(SelectedID);
+                if (bluetoothController.conncetionState == ConncetionSate.sucsesful)
+                {
+                    Context context = Application.Context;
+                    string text = "Успешно!";
+                    ToastLength duration = ToastLength.Short;
+                    var toast = Toast.MakeText(context, text, duration);
+                    toast.Show();
+
+                }   
+                else
+                {
+                    Context context = Application.Context;
+                    string text = "Не удалось подключиться!";
+                    ToastLength duration = ToastLength.Short;
+                    var toast = Toast.MakeText(context, text, duration);
+                    toast.Show();
+                }
+            }
+        }
+
+        private void Spinner_ItemSelected1(object sender, AdapterView.ItemSelectedEventArgs e)
+        {
+            SelectedID = Convert.ToInt32(e.Id);
+        }
+    }
+}
